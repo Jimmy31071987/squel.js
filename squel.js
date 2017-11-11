4071e4c5e6c5409685f27bb8cd7ba9be4d18c25e
Parul Bansal <Jimmy31071987@gmail.com>
2017-11-11 15:08:27 -0500

    added  Case Insensitive search

--- squel.js
+++ squel.js
@@ -9,7 +9,58 @@ Email :jimmy31071987@gmail.com
 function squel() {
 
     this.getItem = getitemByPropertyValue;
- 
+    this.getItemContains = getItemContainsPropertyValue;
+    
+    function getItemContainsPropertyValue(list, propertyKey, propertyValue, caseInsensitive=false) {
+        this.Result = [];
+        if (propertyKey == null || propertyKey == 'undefined' || propertyKey == '') {
+            throw "property is missing";
+        }
+        if (propertyValue == null || propertyValue == 'undefined' || propertyValue == '') {
+            throw "property value is missing";
+        }
+        if (list != null && list != 'undefined' && list != "null") {
+
+            var IsPropertyExists = false;
+            var IsPropertyValueExists = false;
+
+            for (var i = 0; i < list.length; i++) {
+
+                if (list[i] != null) {
+
+                    for (var prop in list[i]) {
+
+                        if (prop == propertyKey) {
+
+                            IsPropertyExists = true;
+
+                            if (caseInsensitive){
+
+                                list[i][prop] = list[i][prop].toLowerCase();
+                                propertyValue = propertyValue.toLowerCase();
+
+                            }
+                            if (list[i][prop].indexOf(propertyValue) >= 0) {
+
+                                IsPropertyValueExists = true;
+
+                                this.Result.push(list[i]);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!IsPropertyExists) {
+                throw "property " + propertyKey + " doesn't exists!"
+            }
+            if (!IsPropertyValueExists) {
+                throw "property doesn't contain value " + propertyValue;
+            }
+        }
+        return this.Result;
+    }
+    
     function getitemByPropertyValue(list, propertyKey, propertyValue) {
         this.Result = null;
         if (propertyKey == null || propertyKey == 'undefined' || propertyKey == '') {
@@ -58,3 +109,6 @@ function squel() {
 
 var Result = new squel().getItem([{ username: "Jim", password: "JimPass" }, { username: "Adam", password: "AdamPass" }, { username: "Rich", password: "RichPass" }], "password", "RichPass");
 console.log(Result);
+Result = new squel().getItemContains([{ username: "Jim", password: "JimPass" }, { username: "Adam", password: "AdamPass" }, { username: "Rich", password: "RichPass" }], "password", "JIM",true);
+console.log(Result);
+
